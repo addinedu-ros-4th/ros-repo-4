4f278c6280a2a2ddf1b35f702ba7ea8367269ffb
seungyeonSeok <162243509+seungyeonSeok@users.noreply.github.com>
2024-05-10 19:34:18 +0900

    Delete test/check_remain_feed/yolo_apply.py

--- test/check_remain_feed/yolo_apply.py
+++
@@ -1,8 +0,0 @@
-#학습 코드 
-from ultralytics import YOLO
-model = YOLO("yolov8m.yaml")
-model = YOLO() 
-model.train(data='data.yaml', epochs=75) #data : data.yaml파일이 위치하는 경로
-
-# 추론 & 바운딩 박스 크롭 저장 
-!yolo task=detect mode=predict model=/home/addinedu/runs/detect/train31/weights/best.pt source=/home/addinedu/Desktop/yolo/qtest/* save=true save_txt=true save_crop=True
