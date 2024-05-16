583b7d0ff54b9d27fc4318001dc69f2b4f061202
dyjung <dyjung.dev@gmail.com>
2024-05-16 15:52:43 +0900

    tcp_test code added

---
+++ test/tcp_test/tcp_test_client.py
@@ -0,0 +1,24 @@
+import socket
+
+# 서버 설정
+server_address = "192.168.0.86"  # 서버의 실제 IP 주소 또는 도메인 이름
+server_port = 3000         # 서버 포트 번호
+
+# 서버에 연결
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_address, server_port))
+
+# 데이터 전송
+name = "미니몬"
+message = "안녕, 서버!"
+
+request = f"{name}&&{message}"
+client_socket.send(request.encode("utf-8"))
+
+# 서버로부터 응답 받기
+response = client_socket.recv(1024).decode("utf-8")
+print(f"{name} : {message}")
+print(f"서버 : {response}\n")
+
+# 클라이언트 소켓 닫기
+client_socket.close()
---
+++ test/tcp_test/tcp_test_server.py
@@ -0,0 +1,47 @@
+import socket
+
+# 서버 설정
+host = "192.168.0.86"  # 서버의 IP 주소 또는 도메인 이름
+port = 3000       # 포트 번호
+
+# 서버 소켓 생성
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(5)
+
+print(f"서버가 {host}:{port}에서 대기 중입니다...")
+
+while True:
+    # 클라이언트 연결 대기
+    client_socket, client_address = server_socket.accept()
+    print(f"클라이언트 {client_address}가 연결되었습니다.")
+    
+    try:
+        # 클라이언트로부터 요청 받기
+        data = client_socket.recv(1024).decode("utf-8")
+        if not data:
+            continue
+
+        # 요청 파싱
+        parts = data.split("&&")
+        if len(parts) != 0:
+            name = parts[0]
+            message = parts[1]
+            response = f"어서와! {name}"
+
+            # 클라이언트 이름과 메시지 출력
+            print(f"클라이언트 이름: {name}")
+            print(f"클라이언트 메시지: {message}")
+        else:
+            response = "유효하지 않은 요청"
+
+        # 응답 클라이언트에게 전송
+        client_socket.send(response.encode("utf-8"))
+
+    except Exception as e:
+        print(f"오류 발생: {e}")
+
+    finally:
+        # 클라이언트 소켓 닫기
+        print("연결종료")
+        client_socket.close()
