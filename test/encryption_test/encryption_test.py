a566d0909f34192b4a5ddaac7d788e216281cfcb
dyjung <dyjung.dev@gmail.com>
2024-05-27 21:54:10 +0900

    encryption test added

---
+++ test/encryption_test/encryption_test.py
@@ -0,0 +1,19 @@
+#pycryptodome
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+# 키와 데이터
+key = get_random_bytes(16)
+data = b"192.168.1.101"
+
+# 암호화
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+print("Cipher text:", ciphertext)
+
+# 복호화
+cipher = AES.new(key, AES.MODE_EAX, nonce=cipher.nonce)
+decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+
+print("Decrypted text:", decrypted_data.decode('utf-8'))
