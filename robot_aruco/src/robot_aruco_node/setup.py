4109c15b4d53609cbebe5cffb574957fa607d74a
junroun <ksg77772000@gmail.com>
2024-06-07 13:16:41 +0900

    robot_aruco_service

---
+++ robot_aruco/src/robot_aruco_node/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_aruco_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jun',
+    maintainer_email='jun@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robot_aruco_node = robot_aruco_node.robot_aruco_node:main'
+        ],
+    },
+)
